15bbdc0dc713ea12180f25a7c9a5594555df0d20
Ruslan Abbasov <abbasov_ruslan1999@icloud.com>
2020-09-07 23:28:24 +0700

    Initial Commit

---
+++ ArtCover/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtCover
+//
+//  Created by Ruslan Abbasov on 07.09.2020.
+//  Copyright © 2020 Ruslan Abbasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
